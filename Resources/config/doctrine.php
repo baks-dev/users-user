e19741fe6cc61e8c89e5b7be0dca0c506b48e0a2
baks.dev <admin@baks.dev>
2023-01-20 18:33:44 +0300

    Baks Development

--- Resources/config/doctrine.php
+++ Resources/config/doctrine.php
@@ -6,35 +6,19 @@ use BaksDev\Users\User\Type\Id\UserUid;
 use BaksDev\Users\User\Type\Id\UserUidType;
 use Symfony\Config\DoctrineConfig;
 
-return static function (ContainerConfigurator $container)
+
+return static function (DoctrineConfig $doctrine)
 {
+	$doctrine->dbal()->type(UserUid::TYPE)->class(UserUidType::class);
 	
-//	$services = $container->services()
-//		->defaults()
-//		->autowire()
-//		->autoconfigure()
-//	;
+	$emDefault = $doctrine->orm()->entityManager('default');
 	
+	$emDefault->autoMapping(true);
 	
-    $container->extension('doctrine', [
-      'dbal' => [
-        'types' =>
-          [
-            UserUid::TYPE => UserUidType::class,
-          ]
-      ],
-	  
-    ]);
-	
-	//$doctrine->dbal()->type(AccountEventUid::TYPE)->class(AccountEventType::class);
-    
-//    $emDefault = $doctrine->orm()->entityManager('default');
-//
-//    $emDefault->autoMapping(true);
-//    $emDefault->mapping('User')
-//      ->type('attribute')
-//      ->dir(__DIR__.'/../../Entity')
-//      ->isBundle(false)
-//      ->prefix('BaksDev\Users\User\Entity')
-//      ->alias('User');
+	$emDefault->mapping('User')
+		->type('attribute')
+		->dir(__DIR__.'/../../Entity')
+		->isBundle(false)
+		->prefix('BaksDev\Users\User\Entity')
+		->alias('User');
 };
