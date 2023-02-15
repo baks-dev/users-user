de87949acbd1758816e93d81483fc15c3f5da1c1
baks.dev <admin@baks.dev>
2023-02-15 16:12:23 +0300

    Baks Development

--- BaksUsersUserBundle.php
+++ BaksUsersUserBundle.php
@@ -16,7 +16,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-class BaksUsersBundle extends AbstractBundle
+class BaksUsersUserBundle extends AbstractBundle
 {
 	
 	public function prependExtension(ContainerConfigurator $container, ContainerBuilder $builder) : void
