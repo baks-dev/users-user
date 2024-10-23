9d1b4c02d613f82843f7ca0ef97fec2b1183e650
kingstar <admin@baks.dev>
2024-10-23 20:36:40 +0300

    Baks Development

--- BaksDevUsersUserBundle.php
+++ BaksDevUsersUserBundle.php
@@ -1,11 +1,24 @@
 <?php
 /*
- * This file is part of the FreshCentrifugoBundle.
- *
- * (c) Artem Henvald <genvaldartem@gmail.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ *  Copyright 2024.  Baks.dev <admin@baks.dev>
+ *  
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is furnished
+ *  to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
  */
 
 declare(strict_types=1);
@@ -22,7 +35,7 @@ class BaksDevUsersUserBundle extends AbstractBundle
 
     public const PATH = __DIR__.DIRECTORY_SEPARATOR;
 
-//    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    //    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     //    {
     //        $services = $container->services()
     //            ->defaults()
